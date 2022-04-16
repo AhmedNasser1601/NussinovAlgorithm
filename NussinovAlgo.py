a42845d45e1cfd0e940476f667b8a51bbefcbaa2
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-04-16 20:12:01 +0200

    Create NussinovAlgo.py
    Update from: Ahmed Nasser

---
+++ NussinovAlgo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 20:07:06 2022
+
+@author: Ahmed Nasser
+"""
+
+
+'''Team Members=>
+        - Ahmed Nasser
+        - Yossef Essam
+        - Maryam Abdulhady
+        - Amany Gamal
+        - Abdulrahman Yossry
+'''
