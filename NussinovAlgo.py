a5f3c751c7b39446165e4cb6ceb33ec90f144b51
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-04-16 20:43:07 +0200

    Update NussinovAlgo.py

--- NussinovAlgo.py
+++ NussinovAlgo.py
@@ -13,3 +13,45 @@ Created on Sat Apr 16 20:07:06 2022
         - Amany Gamal
         - Abdulrahman Yossry
 '''
+
+
+import numpy as np
+
+
+def InitMatrix(N):
+    return
+
+
+def getMaximum():
+    return
+
+
+def checkPairs():
+    return
+
+
+def Indexing():
+    return
+
+
+def fillMatrix():
+    return
+
+
+def traceBack():
+    return
+
+
+
+
+
+'''
+    Input:
+        GGGAAAUCC         No Bifurcation
+        CGGACCCAGACUUUC   Bifurcation
+        UAACGUACUGGAGUA   Bifurcation
+        GGAAUUAGUUAACC    Bifurcation
+'''
+if __name__ == "__main__":
+    rnaSeq = input("Please Enter the RNA Sequence.. ")
+    N = len(rnaSeq)
